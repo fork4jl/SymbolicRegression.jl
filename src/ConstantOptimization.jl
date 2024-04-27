e559b1c0df9db7475c1257e9a5c64c51b8284419
woclass <git@wo-class.cn>
2024-04-26 23:02:53 -0500

    Update ConstantOptimization.jl

--- src/ConstantOptimization.jl
+++ src/ConstantOptimization.jl
@@ -9,31 +9,19 @@ using ..PopMemberModule: PopMember
 function optimize_constants(
     dataset::Dataset{T,L}, member::P, options::Options
 )::Tuple{P,Float64} where {T<:DATA_TYPE,L<:LOSS_TYPE,P<:PopMember{T,L}}
-    dispatch_optimize_constants(dataset, member, options, nothing)
+    dispatch_optimize_constants(dataset, member, options)
 end
 
 function dispatch_optimize_constants(
-    dataset::Dataset{T,L}, member::P, options::Options, idx
+    dataset::Dataset{T,L}, member::P, options::Options
 )::Tuple{P,Float64} where {T<:DATA_TYPE,L<:LOSS_TYPE,P<:PopMember{T,L}}
     nconst = count_constants(member.tree)
     nconst == 0 && return (member, 0.0)
 
-    _optimize_constants(
-        dataset,
-        member,
-        options,
-        options.optimizer_algorithm,
-        options.optimizer_options,
-        idx,
-    )
-end
-
-function _optimize_constants(
-    dataset, member::P, options, algorithm, optimizer_options, idx
-)::Tuple{P,Float64} where {T,L,P<:PopMember{T,L}}
     tree = member.tree
     f(t) = zero(L)
-    result = Optim.optimize(f, tree, algorithm, optimizer_options)
+
+    Optim.optimize(f, tree, options.optimizer_algorithm, options.optimizer_options)
 
     return member, 0.0
 end
