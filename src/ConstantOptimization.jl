34130c8a99406abeb817ab6ad855854441398aca
woclass <git@wo-class.cn>
2024-04-26 23:07:03 -0500

    Update ConstantOptimization.jl

--- src/ConstantOptimization.jl
+++ src/ConstantOptimization.jl
@@ -9,17 +9,17 @@ using ..PopMemberModule: PopMember
 function optimize_constants(
     dataset::Dataset{T,L}, member::P, options::Options
 )::Tuple{P,Float64} where {T<:DATA_TYPE,L<:LOSS_TYPE,P<:PopMember{T,L}}
-    dispatch_optimize_constants(dataset, member, options)
+    _dispatch_optimize_constants(dataset, member, options)
 end
 
-function dispatch_optimize_constants(
+function _dispatch_optimize_constants(
     dataset::Dataset{T,L}, member::P, options::Options
 )::Tuple{P,Float64} where {T<:DATA_TYPE,L<:LOSS_TYPE,P<:PopMember{T,L}}
     nconst = count_constants(member.tree)
     nconst == 0 && return (member, 0.0)
 
     tree = member.tree
-    f(t) = zero(L)
+    f(t) = 0.0
 
     Optim.optimize(f, tree, options.optimizer_algorithm, options.optimizer_options)
 
